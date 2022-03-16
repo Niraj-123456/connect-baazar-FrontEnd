38d27b7a238670e063ab27f478679e2b01e2db21
Niraj Lama <lamaniraj321@gmail.com>
2022-03-16 19:30:11 +0545

    initial commit

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ForgotPassword = () => {
-  return <div>This is forgot password section</div>;
+  return (
+    <div className="fp_container">
+      <div className="fp_main">
+        <h1>Forgot Password</h1>
+        <form>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+          <button type="submit">Send Verification OTP</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ForgotPassword;
